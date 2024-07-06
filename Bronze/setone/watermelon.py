536bba8362e25cf7ed7f1899fba71f557ad3f356
Nidhi Reddy <nidhi@nidhis-air.lan>
2024-07-06 14:24:56 -0700

    restructured

---
+++ Bronze/setone/watermelon.py
@@ -0,0 +1,10 @@
+#https://codeforces.com/problemset/problem/4/A
+# July 6, 2024
+inp = int(input())
+if inp % 2 == 0:
+    if inp == 2:
+        print("NO")
+    else: 
+        print("YES")
+else:
+    print("NO")
